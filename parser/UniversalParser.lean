926cecf8f9ec962b32352f406b3b74b5e3f3b7f7
LQ <234985631+pseudolanguagecompiler@users.noreply.github.com>
2025-12-05 15:55:57 -0500

    Update UniversalParser.lean

--- parser/UniversalParser.lean
+++ parser/UniversalParser.lean
@@ -3,34 +3,34 @@ import Ast.UniversalIR
 
 namespace UniversalParser
 
-open Parser Char
+open Parser
 
--- ID: letter followed by alphanum
-def pID : Parser String := 
-  letter *> (alpha <|> digit)* |>.map fun ls => String.mk' ls.toArray
+-- ID: letter followed by alphanumeric
+def pID : Parser String := letter *> (alpha <|> digit)* |>.map String.join
 
--- Number → Expr.num
-def pNumber : Parser UniversalIR.Expr := 
-  nat.map UniversalIR.Expr.num
+-- Number → Expr
+def pNumber : Parser UniversalIR.Expr := nat.map UniversalIR.Expr.num
 
--- Atom: number | id | ( expr )
-def pAtom : Parser UniversalIR.Expr := pNumber <|> 
-  pID.map UniversalIR.Expr.var <|> 
-  (satisfy (· == '(') *> pExpr <* satisfy (· == ')'))
+-- Atoms: number | id | (expr)
+def pAtom : Parser UniversalIR.Expr := choice [
+  pNumber,
+  pID.map UniversalIR.Expr.var,
+  symbol "(" *> pExpr <* symbol ")"
+]
 
--- Binary expressions (left-associative)
+-- Binary operators (left-associative): + - >
 def pExpr : Parser UniversalIR.Expr := do
   let mut left ← pAtom
   while true do
     if (← test "+") then
       let right ← pAtom
-      left := UniversalIR.Expr.binOp .add left right
+      left := UniversalIR.Expr.binOp UniversalIR.BinOp.add left right
     else if (← test "-") then
       let right ← pAtom
-      left := UniversalIR.Expr.binOp .sub left right
+      left := UniversalIR.Expr.binOp UniversalIR.BinOp.sub left right
     else if (← test ">") then
       let right ← pAtom
-      left := UniversalIR.Expr.binOp .gt left right
+      left := UniversalIR.Expr.binOp UniversalIR.BinOp.gt left right
     else
       break left
   pure left
@@ -39,24 +39,35 @@ def pExpr : Parser UniversalIR.Expr := do
 def pStatement : Parser UniversalIR.Stmt := choice [
   -- set x := expr ;
   do
-    ← keyword "set"
+    keyword "set"
     let x ← pID
-    ← symbol ":="
+    symbol ":="
     let e ← pExpr
-    ← symbol ";"
+    symbol ";"
     pure (UniversalIR.Stmt.set x e),
   
   -- print expr ;
   do
-    ← keyword "print"
+    keyword "print"
     let e ← pExpr
-    ← symbol ";"
-    pure (UniversalIR.Stmt.print e)
+    symbol ";"
+    pure (UniversalIR.Stmt.print e),
+  
+  -- while expr do stmt* end
+  do
+    keyword "while"
+    let cond ← pExpr
+    keyword "do"
+    let body ← many pStatement
+    keyword "end"
+    pure (UniversalIR.Stmt.while cond body)
 ]
 
+-- Full program
 def pProgram : Parser (List UniversalIR.Stmt) := many pStatement
 
+-- Parse entry point
 def parseProgram (input : String) : Except String (List UniversalIR.Stmt) := 
   runParser pProgram input "" |>.toExcept toString
 
-end UniversalParser [web:101][web:102]
+end UniversalParser
